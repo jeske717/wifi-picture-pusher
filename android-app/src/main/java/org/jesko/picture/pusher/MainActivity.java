6e2d3227975f95063f73276f489deba985cc42f2
ben <jeske717@gmail.com>
2012-12-18 23:13:15 -0600

    added a button that launches the native camera

--- android-app/src/main/java/org/jesko/picture/pusher/MainActivity.java
+++ android-app/src/main/java/org/jesko/picture/pusher/MainActivity.java
@@ -1,12 +1,37 @@
 package org.jesko.picture.pusher;
 
-import android.app.Activity;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends RoboActivity {
+	
+	private static final int PICTURE_REQUEST_CODE = 16;
+	
+	@InjectView(R.id.button1)
+	private Button takePictureButton;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		takePictureButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(cameraIntent, PICTURE_REQUEST_CODE);
+			}
+		});
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 }
