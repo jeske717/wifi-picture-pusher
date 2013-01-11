16f1cf59da73617d70b2f5099155863c16e7537c
ben <jeske717@gmail.com>
2013-01-10 20:13:52 -0600

    introduced androidannotations and it is looking pretty good with the amount of code required

--- android-app/src/main/java/org/jesko/picture/pusher/MainActivity.java
+++ android-app/src/main/java/org/jesko/picture/pusher/MainActivity.java
@@ -1,37 +1,21 @@
 package org.jesko.picture.pusher;
 
 import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectView;
 import android.content.Intent;
-import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
 
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(value = R.layout.main)
 public class MainActivity extends RoboActivity {
 	
 	private static final int PICTURE_REQUEST_CODE = 16;
 	
-	@InjectView(R.id.button1)
-	private Button takePictureButton;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		takePictureButton.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				startActivityForResult(cameraIntent, PICTURE_REQUEST_CODE);
-			}
-		});
+	@Click
+	public void takePicture() {
+		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(cameraIntent, PICTURE_REQUEST_CODE);
 	}
 	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-	}
 }
